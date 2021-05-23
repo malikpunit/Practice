92580fbf59a26e35099d10d50cb50a42af30cdf2
Punit Malik <malikpunit@ymail.com>
2021-05-23 13:53:59 +0530

    Create EvenlySpaced.java

---
+++ EvenlySpaced.java
@@ -0,0 +1,43 @@
+class EvenlySpaced{
+
+  public boolean evenlySpaced(int a, int b, int c) {
+  //1. sort them
+  //2. check diff(medium-small) == (large-medium)
+  
+  if(a>=b){
+    if(a>=c){
+      if(b>=c){
+        //a>b>c
+        return checkEvenlySpacedSorted(a,b,c);
+      } else{
+        //a>c>b
+        return checkEvenlySpacedSorted(a,c,b);
+      }
+      
+    } else{
+      //c>a>b
+      return checkEvenlySpacedSorted(c,a,b);
+    }
+  } else {
+    if(b>=c){
+      if(a>=c){
+        //b>a>c
+        return checkEvenlySpacedSorted(b,a,c);
+        
+      } else{
+        //b>c>a
+        
+        return checkEvenlySpacedSorted(b,c,a);
+      }
+    } else{
+      //c>b>a
+      return checkEvenlySpacedSorted(c,b,a);
+    }
+  }
+}  
+
+private boolean checkEvenlySpacedSorted(int a, int b, int c){
+  return (a-b)==(b-c);
+}
+
+}
