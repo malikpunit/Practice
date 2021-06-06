e916bf6b0eb908a231c5fbd5ac0e262756c92b29
Punit Malik <malikpunit@ymail.com>
2021-06-06 13:51:07 +0530

    Update IsomorphicStrings.java

--- IsomorphicStrings.java
+++ IsomorphicStrings.java
@@ -1,6 +1,8 @@
 class IsomorphicStrings {
    //https://leetcode.com/explore/learn/card/hash-table/184/comparison-with-other-data-structures/1117/
+    
     //Approach -1 using HashMaps
+    
     public boolean isIsomorphicByHashMap(String s, String t){
         //charIn(s) -> charIn(t)
         Map<Character, Character> mappingS2T = new HashMap<>();
@@ -22,15 +24,16 @@ class IsomorphicStrings {
         }
         return true;
     }
-  
-  
-    public boolean isIsomorphic(String s, String t) {
+    
+    
+    //Approach -2 based on index approach https://www.geeksforgeeks.org/check-if-two-given-strings-are-isomorphic-to-each-other
+    public boolean isIsomorphicArray(String s, String t) {
         //traversedS[charOf(s)] ->  charOf(t) 
         int[] traversedS = new int[256];
         Arrays.fill(traversedS, -1);
         
         
-        //traversedT[charOf(t)] -> true
+        //traversedT[charOf(t)] -> charOf(s)
         int[] traversedT = new int[256];     
         Arrays.fill(traversedT, -1);
         
@@ -49,5 +52,40 @@ class IsomorphicStrings {
         }
         return true;        
     }    
+    
+    //Approach -3 based on count approach https://www.geeksforgeeks.org/check-if-two-given-strings-are-isomorphic-to-each-other
+    //will fail on "bbbaaaba", "aaabbbba"
+    public boolean isIsomorphicCount(String s, String t) {
+        //traversedS[charOf(s)] -> charOf(t)
+        int[] traversedS = new int[256];
+        Arrays.fill(traversedS, -1);
+        
+        
+        //traversedT[charOf(t)] -> charOf(s)
+        int[] traversedT = new int[256];     
+        Arrays.fill(traversedT, -1);
+        
+        int len=s.length();
+        
+        for(int i=0; i<len; i++){
+            int charS = s.charAt(i);
+            int charT = t.charAt(i);
+            traversedS[charS] = traversedS[charS] + 1;
+            traversedT[charT] = traversedT[charT] + 1;
+            
+            if(traversedS[charS] != traversedT[charT]){
+                return false;
+            }
+            
+        }
+        return true;
+    }
+      
+    public boolean isIsomorphic(String s, String t) {
+        //return isIsomorphicByHashMap(s, t);
+        return isIsomorphicArray(s, t);
+        //return isIsomorphicCount(String s, t);
+    }
+    
     
 }
